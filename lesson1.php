4998fcb9f23f2b7cf4f917456eba7124e0cc36d8
PROaction <sergey1404sergey@mail.ru>
2016-03-09 17:34:17 +0300

    lesson1

---
+++ lesson1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XMHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <title>Peremennie</title>
+ </head>
+ 
+ <body>
+ <?php 
+ 
+  $car = "BMW";
+  $model = 525;
+  $test = "test";
+  
+  //echo "Buy $car! <br />";
+  //echo date('H:i');
+ 
+ /*
+ $a = $_GET['page'];
+ $b = $_GET['article'];
+ if($page == 1 && $article == 2){
+    echo "Da";
+ }
+ */
+ 
+ 
+ 
+ ?>
+
+ 
+ 
+ 
+ </body>
+ </html>
